64663493721694c0b40b2242688e564f71a02ff3
Vladislav Pyatnitskiy <vlad061295@yahoo.com>
2026-06-21 13:27:04 +0500

    Refactor moex_data to handle multiple tickers

--- moex data.py
+++ moex data.py
@@ -1,27 +1,40 @@
 import requests
 import pandas as pd
 
-def moex_data(x, s, e):
+def moex_data(x, s, e): # Moex Data
   
-  url = (
-    f"https://iss.moex.com/iss/engines/stock/"
-    f"markets/shares/securities/{x}/candles.json"
-    )
+  if isinstance(x, str):
+    x = [x]
+  
+  dfs = []
+  
+  for ticker in x:
+    
+    url = (
+      f"https://iss.moex.com/iss/engines/stock/"
+      f"markets/shares/securities/{ticker}/candles.json"
+      )
+      
+    params = {
+        "from": s,
+        "till": e,
+        "interval": 24
+    }
+    
+    data = requests.get(url, params=params).json()
+    
+    columns = data["candles"]["columns"]
+    rows = data["candles"]["data"]
+    
+    df = pd.DataFrame(rows, columns=columns)
+    
+    df["Date"] = pd.to_datetime(df["begin"]).dt.date
+    
+    df = df[["close", "Date"]].set_index("Date").rename(
+      columns={"close": ticker})
+      
+    dfs.append(df)
+  
+  return pd.concat(dfs, axis=1)
 
-  params = {
-      "from": s,
-      "till": e,
-      "interval": 24
-  }
-  
-  r = requests.get(url, params=params)
-  data = r.json()
-  
-  columns = data["candles"]["columns"]
-  rows = data["candles"]["data"]
-  
-  df = pd.DataFrame(rows, columns=columns)
-  
-  return df
-
-moex_data("SBER", "2024-01-01", "2024-12-31")
+moex_data(["SBER", "GAZP"], "2024-01-01", "2024-12-31")
